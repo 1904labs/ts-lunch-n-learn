a1732291582fcc58cc55ee22ec096a9644a76fd4
AJ Srivastava <anuj@four7.us>
2019-06-16 12:23:08 -0500

    Saving changes

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,51 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
 
-// Create an interface to strongly type the props and state for a stateful component
-interface IAppState {
-    count: number;
-}
-
-interface IAppProps {
+interface ICommentCardProps {
     name: string;
+    ts: string;
+    comment: string;
+    imgUrl: string;
 }
 
-class App extends React.Component<IAppProps, IAppState> {
-    public state: IAppState = {
-        count: 0
-    };
+export default class App extends React.Component {
 
-    increment() {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
-    decrement() {
-        this.setState({
-            count: this.state.count - 1
-        })
-    }
-
-    public render() {
+    render() {
         return (
-            <div className="App">
-                Hello {this.props.name}!
-                <StatelessFC count={this.state.count}/>
-                <button onClick={() => this.decrement()}>Decrement</button>
-                <button onClick={this.increment.bind(this)}>Increment</button>
+            <div className="app">
+                <Counter />
             </div>
-        );
+        )
     }
 }
 
-interface IStatelessFCProps {
-    count: number;
-} 
-
-const StatelessFC = (props: IStatelessFCProps) => {
+const Counter = () => {
+    const [ name, setName ] = useState('AJ');
     return (
-        <div>Count: {props.count}</div>
+        <div>
+            <input onChange={(e) => setName(e.target.value)} value={name} />
+            <div>
+                {name}
+            </div>
+        </div>
     )
-};
+}
 
-export default App;
---
+++ typescript-example/fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz(n) {
+    for (var i = 0; i < n; i++) {
+        if (i % 5 === 0 && i % 3 === 0) {
+            console.log('fizzbuzz');
+        }
+        else if (i % 5 === 0) {
+            console.log('buzz');
+        }
+        else if (i % 3 === 0) {
+            console.log('fizz');
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+fizzbuzz(100);

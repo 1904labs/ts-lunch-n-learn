69b4f6ed5300acdbb5c018c757501d9777763d7d
AJ Srivastava <anuj@four7.us>
2019-06-19 16:07:53 -0500

    add stuff before l&l

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,27 +1,66 @@
-import React, {useState} from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import './App.scss';
-import {Todos} from "./components/Todos";
+// import {Todos} from "./components/Todos";
 
-interface ICommentCardProps {
-    name: string;
-    ts: string;
-    comment: string;
-    imgUrl: string;
-}
+// 1. Classic Class Component
+// 2. Classic Pure Component
+// 3. Const SFC
+// 4. Hooks Example Text
+// 5. Hooks Example Number
 
 export default class App extends React.Component {
-
     render() {
         return (
             <div className="app">
-                <Todos title="Testing Typescript" />
-                {/*<Counter />*/}
+                <ClassComponent title="Typescript Lunch & Learn" version={0.1} />
+                {/*<Todos title="Testing Typescript" />*/}
+                <HookExample initialCount={47} />
             </div>
         )
     }
 }
 
-const Counter = () => {
+interface ClassProps {
+    title: string;
+    version: 0.1;
+}
+
+class ClassComponent extends React.Component<ClassProps, {}>{
+    render() {
+        return (
+            <div>
+                <div>{this.props.title} - {this.props.version}</div>
+                <H3Title text="H3 Title Component" />
+                {/*<CounterClass count={0} />*/}
+                <SFCExample text="AJ Srivastava"/>
+                <InputSFC/>
+            </div>
+        )
+    }
+}
+
+interface H3TTitleProps {
+    text: string;
+}
+
+// Class component
+class H3Title extends React.PureComponent<H3TTitleProps> {
+    render() {
+        return (
+            <h3>{this.props.text}</h3>
+        )
+    }
+}
+
+// Stateless Functional Component as function
+const SFCExample: FunctionComponent<H3TTitleProps> = (props: H3TTitleProps) => {
+    return (
+        <h3>{props.text}</h3>
+    )
+};
+
+
+const InputSFC = (props: any) => {
     const [ name, setName ] = useState('AJ');
     return (
         <div>
@@ -31,5 +70,19 @@ const Counter = () => {
             </div>
         </div>
     )
+};
+
+interface HookExampleProps {
+    initialCount: number;
 }
 
+const HookExample = (props: HookExampleProps) => {
+    const [ count, setCount ] = useState<number>(props.initialCount);
+    return (
+        <div>
+            <span>{count}</span>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
+    )
+}
--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -36,6 +36,7 @@ export class Todos extends React.Component<IProps, IState> {
     render() {
         return (
             <div>
+                <Test name="AJ wuz here" />
                 {this.props.title}
                 {this.state.todos.length > 0 && this.state.todos.map((todo: ITodo) => {
                         return (
@@ -46,6 +47,29 @@ export class Todos extends React.Component<IProps, IState> {
                 <input type="text" onChange={(e) => this.setState({inputText: e.target.value})} value={this.state.inputText} />
                 <button onClick={(e) => this.submitTodo()}>Submit</button>
             </div>
+        )
+    }
+}
+
+interface PureProps {
+    name: string;
+    bleh?: NestedType;
+}
+
+interface NestedType {
+    set: Set<number>;
+}
+
+
+export class Test extends React.PureComponent<PureProps> {
+    constructor(props: PureProps) {
+        super(props);
+        const s = new Set<number>();
+        s.add(1);
+    }
+    render() {
+        return (
+            <div className="pure">Name: {this.props.name}</div>
         )
     }
 }

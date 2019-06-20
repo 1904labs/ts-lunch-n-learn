e4e1a1da9394d2770eda56378775aea5b5f2345a
Tynarus <admin@tynarus.com>
2019-06-20 11:09:57 -0500

    Fixing some angular stuff

--- angular-ts/src/app/app.component.ts
+++ angular-ts/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'labs-root',
     templateUrl: './app.component.html',
     styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-    title = 'angular-ts';
+export class AppComponent implements OnInit {
+
+    private _title: string = 'angular-ts';
+    public list: [ string, string, number ] = [ 'My', 'List', 2 ];
+
+    public constructor() {
+    }
+
+    public ngOnInit(): void {
+    }
+
+    public get title() {
+        return this._title;
+    }
+
 }

6119b2ca983b2369a427020ea25a3495d814ddd0
AJ Srivastava <anuj@four7.us>
2019-06-20 09:29:20 -0500

    major cleanup/refactor

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,7 +1,8 @@
 import React, {FunctionComponent, useState} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import {WelcomeScreen, WelcomeScreenProps} from "./components/WelcomeScreen";
-// import {Todos} from "./components/Todos";
+import {TodoList} from "./components/TodoList";
 
 // 1. Class Component
 // 2. Pure Component
@@ -17,97 +18,60 @@ export default class App extends React.Component {
             timestamp: new Date().toLocaleString()
         };
         return (
-            <div className="app">
-                <WelcomeScreen {...WSProps} />
-                {/*<ClassComponent title="Typescript Lunch & Learn" version={0.1} />*/}
-                {/*<Todos title="Testing Typescript" />*/}
-                {/*<HookExample initialCount={47} />*/}
-            </div>
+            <Router>
+                <div className="app">
+                    <Route path="/welcome" component={() => {
+                        return (
+                            <WelcomeScreen {...WSProps} />
+                        )
+                    }} />
+                    <Route path="/class" component={SampleClassComponent} />
+                    <Route path="/pure" component={SamplePureComponent} />
+                    <Route path="/sfc" component={SampleSFC} />
+                    <Route path="/hooks" component={SampleHook} />
+                    <Route path="/todos" component={TodoList} />
+                </div>
+            </Router>
         )
     }
 }
 
-interface ClassProps {
-    title: string;
-    version: 0.1;
-}
-
-interface ClassState {
-    loading: boolean;
-}
-
-class ClassComponent extends React.Component<ClassProps, ClassState>{
-    state: ClassState;
-    constructor(props: ClassProps) {
-        super(props);
-        this.state = {
-            loading: true
-        }
+class SampleClassComponent extends React.Component {
+    render() {
+        return (
+            <h3>Sample Component Works!</h3>
+        )
     }
+}
+
+class SamplePureComponent extends React.PureComponent {
     render() {
         return (
             <div>
-                <div>{this.props.title} - {this.props.version}</div>
-                <div>Loading: {this.state.loading ? 'True' : 'False'}</div>
-                <H3Title text="H3 Title Component" />
-                {/*<CounterClass count={0} />*/}
-                <SFCExample text="AJ Srivastava"/>
-                <InputSFC/>
+                <h3>Sample Pure Component Works</h3>
+                <a href="https://codeburst.io/when-to-use-component-or-purecomponent">What's the difference?</a>
+                <p><em>PureComponent handles 'shouldComponentUpdate' for you</em></p>
             </div>
         )
     }
 }
 
-interface HTitleProps {
-    text: string;
-}
-
-// Class Pure Component
-export class H3Title extends React.PureComponent<HTitleProps> {
-    render() {
-        return (
-            <h3>{this.props.text}</h3>
-        )
-    }
-}
-
-export const H1Title = (props: HTitleProps) => {
+const SampleSFC: FunctionComponent = () => {
     return (
-        <h1>{props.text}</h1>
+        <h3>Sample Stateless Functional Component Works!</h3>
     )
 };
 
-// Stateless Functional Component as function
-const SFCExample: FunctionComponent<HTitleProps> = (props: HTitleProps) => {
-    return (
-        <h3>{props.text}</h3>
-    )
-};
-
-
-const InputSFC = (props: any) => {
-    const [ name, setName ] = useState('AJ');
+const SampleHook: FunctionComponent = () => {
+    const [ str, setStr ] =  useState('1904labs');
+    const [ count, setCount ] = useState(0);
     return (
         <div>
-            <input onChange={(e) => setName(e.target.value)} value={name} />
-            <div>
-                {name}
-            </div>
-        </div>
-    )
-};
-
-interface HookExampleProps {
-    initialCount: number;
-}
-
-const HookExample = (props: HookExampleProps) => {
-    const [ count, setCount ] = useState<number>(props.initialCount);
-    return (
-        <div>
-            <span>{count}</span>
+            <h1>{str}</h1>
+            <h1>Count {count}</h1>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
         </div>
+
     )
-}
+};
